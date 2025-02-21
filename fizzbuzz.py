41698b54bb0f6243d4e05d0f000567fef2b61f57
Estrella Razo <92348246+EstrellaRazo@users.noreply.github.com>
2025-02-21 16:12:21 -0600

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,21 @@
+"""
+ EL FAMOSO FIZZBUZZ
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+"""
+def fizzbuzz(num):
+    for n in range(1, num+1):
+        if n%3==0 and n%5!=0:
+            print("fizz")
+        elif n%5==0 and n%3!=0:
+            print("buzz")
+        elif n%3==0 and n%5==0:
+            print("fizzbuzz")
+        else:
+            print(n)
+        
+fizzbuzz(15)
